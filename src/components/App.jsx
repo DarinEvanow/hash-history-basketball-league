478dd7f1aff747cb49a3695712b430c9db3110b8
Darin <darinevanow@gmail.com>
2018-06-07 15:58:35 -0700

    Created basic home, players, teams components, and the routes to access them

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import Players from './Players';
+import Teams from './Teams';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        React Router Project
-      </div>
+      <Router>
+        <div>
+          <Route path='/' exact component={Home} />
+          <Route path='/players' exact component={Players} />
+          <Route path='/teams' exact component={Teams} />
+        </div>
+      </Router>
     );
   }
 }
