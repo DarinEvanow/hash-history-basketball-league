987794528b933bd32acd0ac67510ea835626dc92
Darin <darinevanow@gmail.com>
2018-06-07 16:02:44 -0700

    Added basic navbar to go to the different routes and components alread reated.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,15 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
 import Players from './Players';
 import Teams from './Teams';
+import Navbar from './Navbar';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+          <Navbar />
+
           <Route path='/' exact component={Home} />
           <Route path='/players' exact component={Players} />
           <Route path='/teams' exact component={Teams} />
