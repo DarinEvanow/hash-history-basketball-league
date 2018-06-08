8b91f8d5c7855bce13055612cd6f79105fde3aa8
Darin <darinevanow@gmail.com>
2018-06-08 16:04:20 -0700

    Added 404 page for unmatched routes.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Players from './Players';
 import Teams from './Teams';
 import Navbar from './Navbar';
+import ErrorPage from './ErrorPage';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Navbar />
-
-          <Route path='/' exact component={Home} />
-          <Route path='/players' exact component={Players} />
-          <Route path='/teams' exact component={Teams} />
+        <Navbar />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/players' component={Players} />
+            <Route path='/teams' component={Teams} />
+            <Route component={ErrorPage} />
+          </Switch>
         </div>
       </Router>
     );
