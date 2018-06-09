c97b3886450311ee81a745f9620532fc2cb34943
Darin <darinevanow@gmail.com>
2018-06-09 11:51:02 -0700

    Added logic to Players component in order to fetch our data, and currently it is just printing the stringified JSON to the page.

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { parse } from 'query-string';
+import { slug } from 'slug';
+import { getPlayers } from '../api';
+import Sidebar from './Sidebar';
 
 export default class Players extends Component {
+  state = {
+    players: [],
+    loading: true,
+  }
+
+  componentDidMount () {
+    const { location } = this.props
+
+    location.search
+      ? this.fetchPlayers(parse(location.search).teamId)
+      : this.fetchPlayers()
+  }
+
+  fetchPlayers = (teamId) => {
+    getPlayers(teamId)
+      .then((players) => this.setState(() => ({
+        players,
+        loading: false,
+      })))
+  }
+
   render() {
     return (
       <div className='container two-column'>
-        Players
+        {JSON.stringify(this.state)}
       </div>
     )
   }
