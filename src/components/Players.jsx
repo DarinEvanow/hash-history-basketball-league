0ea384e0865d746f71a2dc0af43dd3b08bd10c8c
Darin <darinevanow@gmail.com>
2018-06-11 21:28:08 -0700

    Fetch player information and render it when a player is selected on the sidebar

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import { parse } from 'query-string';
-import { slug } from 'slug';
+import slug from 'slug';
 import { getPlayers } from '../api';
 import Sidebar from './Sidebar';
 
+function Player ({ name, position, teamId, number, avatar, apg, ppg, rpg, spg }) {
+  return (
+    <div className='panel'>
+      <img className='avatar' src={`${avatar}`} alt={`${name}'s avatar`} />
+      <h1 className='medium-header'>{name}</h1>
+      <h3 className='header'>#{number}</h3>
+      <div className='row'>
+        <ul className='info-list' style={{marginRight: 80}}>
+          <li>Team
+            <div>
+              <Link style={{color: '#68809a'}} to={`/${teamId}`}>
+                {teamId[0].toUpperCase() + teamId.slice(1)}
+              </Link>
+            </div>
+          </li>
+          <li>Position<div>{position}</div></li>
+          <li>PPG<div>{ppg}</div></li>
+        </ul>
+        <ul className='info-list'>
+          <li>APG<div>{apg}</div></li>
+          <li>SPG<div>{spg}</div></li>
+          <li>RPG<div>{rpg}</div></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
 export default class Players extends Component {
   state = {
-    players: [],
     loading: true,
+    players: [],
   }
 
   componentDidMount () {
     const { location } = this.props
-
     location.search
       ? this.fetchPlayers(parse(location.search).teamId)
       : this.fetchPlayers()
@@ -22,28 +49,35 @@ export default class Players extends Component {
   fetchPlayers = (teamId) => {
     getPlayers(teamId)
       .then((players) => this.setState(() => ({
-        players,
         loading: false,
+        players,
       })))
   }
 
-  render() {
+  render () {
     const { players, loading } = this.state;
     const { match, location } = this.props;
 
     return (
       <div className='container two-column'>
-        <Sidebar 
+        <Sidebar
           loading={loading}
           title='Players'
-          list={players.map(((player) => player.name))}
+          list={players.map((player) => player.name)}
           {...this.props}
         />
 
         {loading === false && location.pathname === '/players'
           ? <div className='sidebar-instruction'>Select a Player</div>
-          : null
-        }
+          : null}
+
+        <Route path={`${match.url}/:playerId`} render={({match}) => {
+          if (loading === true) return null
+
+          return (
+            <Player {...players.find((player) => slug(player.name) === match.params.playerId)} />
+          )
+        }} />
       </div>
     )
   }
