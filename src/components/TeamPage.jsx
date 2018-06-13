61ca8c8a77231e6e4c1e2521e721ff590938a9f7
Darin <darinevanow@gmail.com>
2018-06-13 14:19:59 -0700

    Have logo and team name rendering on the team page.

--- src/components/TeamPage.jsx
+++ src/components/TeamPage.jsx
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import slug from 'slug';
+import { getTeamsArticles } from '../api';
+import Team from './Team';
+import TeamLogo from './TeamLogo';
 
 export default class TeamPage extends Component {
+  state = {
+    loading: true,
+    articles: [],
+  }
+
+  componentDidMount () {
+    getTeamsArticles(this.props.match.params.teamId)
+      .then((articles) => {
+        this.setState(() => ({
+          loading: false,
+          articles,
+        }))
+      })
+  }
+
   render() {
+    const { loading, articles } = this.state;
+    const { match } = this.props;
+    const { teamId } = match.params;
+
     return (
       <div>
-        TeamPage
+        <Team id={teamId}>
+          {(team) => team === null
+            ? <h1>Loading</h1>
+            : <div className='panel'>
+                <TeamLogo id={teamId} />
+                <h1 className='medium-header'>{team.name}</h1>
+              </div>}
+        </Team>
       </div>
     )
   }
