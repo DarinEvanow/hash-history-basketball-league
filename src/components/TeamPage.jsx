fdda9f54783a81b2136b9414bf8872d67eeb590d
Darin <darinevanow@gmail.com>
2018-06-13 14:41:06 -0700

    Rendered all articles of a team to the page.

--- src/components/TeamPage.jsx
+++ src/components/TeamPage.jsx
@@ -34,6 +34,36 @@ export default class TeamPage extends Component {
             : <div className='panel'>
                 <TeamLogo id={teamId} />
                 <h1 className='medium-header'>{team.name}</h1>
+                <h4 style={{margin: 5}}>
+                  <Link
+                    style={{cursor: 'pointer'}}
+                    to={{pathname: '/players', search: `?teamId=${teamId}`}}
+                  >
+                    View Roster
+                  </Link>
+                </h4>
+                <ul className='championships'>
+                  {team.championships.map((championship) => <li key={championship}>{championship}</li>)}
+                </ul>
+                <ul className='info-list row' style={{width: '100%'}}>
+                <li>Established<div>{team.established}</div></li>
+                  <li>Manager<div>{team.manager}</div></li>
+                  <li>Coach<div>{team.coach}</div></li>
+                  <li>Record<div>{team.wins}-{team.losses}</div></li>
+                </ul>
+                <h2 className='header'>
+                  Articles
+                </h2>
+                <ul className='articles'>
+                  {articles.map((article) => (
+                    <li key={article.id}>
+                      <Link to={`${match.url}/articles/${slug(article.title)}`}>
+                        <h4 className='article-title'>{article.title}</h4>
+                        <div className='article-date'>{article.date.toLocaleDateString()}</div>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
               </div>}
         </Team>
       </div>
