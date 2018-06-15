7cf5e6854a14e4fcc3fc0d0af40728a49f21c1c0
Darin <darinevanow@gmail.com>
2018-06-15 14:05:23 -0700

    Added a reusable loading component, and replaced all Loading headers with said component

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import { getTeamsArticles } from '../api';
 import Sidebar from './Sidebar';
 import Article from './Article';
+import Loading from './Loading';
 
 export default class Articles extends Component {
   state = {
@@ -26,7 +27,7 @@ export default class Articles extends Component {
     const { teamId } = params;
 
     return loading === true
-      ? <h1>Loading</h1>
+      ? <Loading />
       : <div className='container two-column'>
         <Sidebar
           loading={loading}
@@ -37,7 +38,7 @@ export default class Articles extends Component {
 
         <Route path={`${url}/:articleId`} render={({ match }) => (
           <Article teamId={teamId} articleId={match.params.articleId}>
-            {(article) => !article ? <h1>Loading</h1> : (
+            {(article) => !article ? <Loading /> : (
               <div className='panel'>
                 <article className='article' key={article.id}>
                   <h1 className='header'>{article.title}</h1>
